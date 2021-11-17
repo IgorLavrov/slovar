1693c332d1a6b19bc8a67b048b175f9db1239128
Lavrovi <igor.lavrov90@hotmail.co.uk>
2021-11-17 18:36:44 +0200

    shange

--- module1.py
+++ module1.py
@@ -2,23 +2,61 @@ def translate_list(f,t,str):
     N=len(f)
     w=f.count(str)
     w2=t.count(str)
-    if w or w2 ==0:
-        add(f,t)
-    elif w or w2!=0:
+    if w==0 and w2 ==0:
+       valikyes=input("No such  word , would you like to add it. please enter yes or no: ")
+       if valikyes.lower()=="yes":
+               add(f,t)
+       elif valikyes.lower()=="no":
+             quit() 
+    elif w!=0 and w2!=0:
         for i in range(N):
-            if f[i]== t[i] or t[i]==f[i]:
-                print(f[i],"-->",t[i])
-            elif f[i]!=t[i] or t[i]!=f[i]:
-                print(" wrong")
+            if str== t[i] or str==f[i]:
+                print(f[i]," meaning-->",t[i])
+    elif str!=t[i] or str!=f[i]:
+       print(" wrong word")
       
 def add(f,t):
     text2=input("напишите Слово на русском")
     with  open("TextFile1.txt",'a') as h:
            h.write("\n"+text2)
-    text3=input("напишите Слово на estonian")
+    text3=input(" write this word on estonian")
     with  open("TextFile2.txt",'a') as g:
                 g.write("\n"+text3)
     return f and t
+
+def change(f,t,str):
+     w3=f.count(str)
+     w4=t.count(str)
+     if w3 >=1 or w4>=1:
+        g = open("TextFile1.txt","r+")
+        d = g.readlines()
+        g.seek(0)
+        c = open("TextFile2.txt","r+")
+        h = c.readlines()
+        c.seek(0)
+        n=0
+        for i in d :
+            if i.rstrip('\n')!= str :
+                g.write(i)
+                g.truncate() 
+            elif i.rstrip('\n')== str:
+               n=d.index(i)
+        g.close()
+        print(n)
+        for b in h:
+            if h.index(b)!=n:
+                c.write(b)
+                c.truncate()        
+        c.close()
+       
+     else:   
+        p=input("no such word, would you like to add it yes/no:")
+        if p.lower()=="yes":
+              add(f,t)
+        elif p.lower()=="no":
+             quit() 
+            
+
 
 def loe_failist(f):
     fail=open(f,'r',encoding="utf-8-sig")
--- slovar.py
+++ slovar.py
@@ -10,8 +10,10 @@ while 1:
     print("a-перевод с английского языка на русский и с русского на английский,\ne-добавить слово в словарь,\nr-исправить слово,\nt-проверить знание слов из словаря")
     valik=input("Ваш выбор:")
     if valik.lower()=="a":
-      translate_list(rus_list,est_list,str)
+      translate_list(rus_list,est_list,word)
     elif valik.lower()=="e":
        rus_list,est_list=add(rus_list,est_list)
     elif valik.lower()=="r":
+        change(rus_list,est_list,word)
+    elif valik.lower()=="t":
         break
